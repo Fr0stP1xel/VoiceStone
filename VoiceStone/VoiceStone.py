8206b863ad407ac31c3c4e095d1a59c61a966c5c
Fr0stP1xel <namit.s14@gmail.com>
2020-02-02 14:53:27 -0800

    Improved screen grabber performance

--- VoiceStone/VoiceStone.py
+++ VoiceStone/VoiceStone.py
@@ -1,14 +1,16 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
 
+lastTime = time.time()
 while(True):
-    printScreen_PIL = ImageGrab.grab(bbox = (0, 40, 800, 640))
-    printScreen_Numpy = np.array(printScreen_PIL.getdata(), dtype = 'uint8')\
-                          .reshape((printScreen_PIL.size[1], printScreen_PIL.size[0], 3))
+    screen = np.array(ImageGrab.grab(bbox = (0, 40, 800, 640)))
 
-    cv2.imshow('window', printScreen_Numpy)
+    print('Loop took {} seconds'.format(time.time() - lastTime))
+    lastTime = time.time()
 
+    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     if(cv2.waitKey(25) & 0xFF == ord('q')):
         cv2.destroyAllWindows()
         break
