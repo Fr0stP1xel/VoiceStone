1434b3c9bb51ef97ea0e23425a16c7651f7b38c7
Fr0stP1xel <namit.s14@gmail.com>
2020-02-02 14:33:10 -0800

    Added screen grabber

--- VoiceStone/VoiceStone.py
+++ VoiceStone/VoiceStone.py
@@ -1,1 +1,14 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
 
+while(True):
+    printScreen_PIL = ImageGrab.grab(bbox = (0, 40, 800, 640))
+    printScreen_Numpy = np.array(printScreen_PIL.getdata(), dtype = 'uint8')\
+                          .reshape((printScreen_PIL.size[1], printScreen_PIL.size[0], 3))
+
+    cv2.imshow('window', printScreen_Numpy)
+
+    if(cv2.waitKey(25) & 0xFF == ord('q')):
+        cv2.destroyAllWindows()
+        break
